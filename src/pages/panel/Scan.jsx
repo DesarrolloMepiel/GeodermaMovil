dee72e16f7ed787742788f54c884d2ee14c63bb0
alejandrowaldo.com <sistemasdevjr@gmail.com>
2022-10-17 14:56:27 -0500

    prueba camaras

--- src/pages/panel/Scan.jsx
+++ src/pages/panel/Scan.jsx
@@ -2,6 +2,10 @@ const Scanner = () => {
     return (
         <>
         <h2>Scanner</h2>
+        <a href="//parzibyte.me/blog">By Parzibyte</a>
+          <p id="resultado">Aquí aparecerá el código</p>
+          <p>A continuación, el contenedor: </p>
+          <div id="contenedor"></div>
         </>
     );
 }
